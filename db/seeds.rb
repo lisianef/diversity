42d993fc1b20a705d82456fac2d6cd76a0ce930b
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2021-06-22 23:23:51 +0200

    Create seeds with export csv + index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+CSV.foreach(Rails.root.join('lib/tasks/goodreads_library_export.csv'), headers: true) do |row|
+
+    Book.create({
+        title: row[1],
+        pages: row[11],
+        publication_year: row[12],
+        ISBN: row[6],
+        status: row[18]
+
+    })
+
+end
+
+
+# t.string "title"
+#     t.integer "pages"
+#     t.integer "publication_year"
+#     t.integer "cover"
+#     t.integer "ISBN"
+#     t.string "status"
+#     t.string "goodreads_link"
